5b956bce9a21c17d79e78a1d1f6b4ae60e950d9a
genarvaez <ge_naravez@hotmail.com>
2017-05-09 10:35:08 -0300

    funcionalidades termiandas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,37 @@
+class Planetas
+{
+	constructor (nombre, peso)
+	{
+		this.nombre = nombre;
+		this.peso = peso;
+		this.calculo = function (tierra){ return tierra * this.peso}
+	}
+}
 
+
+var jupiter = new Planetas ("Júpiter", 2.55);
+var venus = new Planetas ("Venus", 0.87);
+var urano = new Planetas ("Urano", 0.99);
+var marte = new Planetas ("Marte", 0.38);
+var mercurio = new Planetas ("Mercurio", 0.39);
+var saturno = new Planetas ("Saturno", 0.93);
+var neptuno = new Planetas ("Neptuno", 1.38);
+var luna = new Planetas ("Luna", 0.17);
+var resultado = document.getElementById("contenido");
+
+
+function calcular ()
+{
+	printHTML("")
+	var arrPlanetas = [jupiter,venus,urano,marte,mercurio,saturno,neptuno,luna];
+	var peso = document.getElementById("pesoTierra").value;
+	arrPlanetas.forEach(function(elemento){ return resultado.innerHTML += elemento.nombre + " " + "<input type='text' value='" 
+		+ elemento.calculo(peso) + "'>" + "<br><br>"})
+	document.getElementById("pesoTierra").value = "";
+}
+
+function printHTML (html)
+{
+  resultado.innerHTML = '';
+  resultado.innerHTML = html;
+}
